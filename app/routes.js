eed359d0dcade2089b303b8f3f098b04959e7856
lakshmi Karuppiah <lakshmi.karuppiah@nhs.net>
2021-05-05 10:35:27 +0100

    s1 changes

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,9 @@ router.use('/version-3', require('./views/version-3/_routes'));
 router.use('/version-4', require('./views/version-4/_routes'));
 router.use('/version-5', require('./views/version-5/_routes'));
 router.use('/version-6', require('./views/version-6/_routes'));
-router.use('/version-6', require('./views/version-7/_routes'));
+router.use('/version-7', require('./views/version-7/_routes'));
+router.use('/version-8', require('./views/version-8/_routes'));
+router.use('/version-9', require('./views/version-9/_routes'));
 
 
 module.exports = router;
---
+++ app/views/version-9/_routes.js
@@ -0,0 +1,68 @@
+// External dependencies
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json()); // to support JSON bodies
+router.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+
+//account-requests-process.html
+router.post('/select-decision', function (req, res) {
+
+  const selectedRadio = req.body.accountReq;
+
+  if (selectedRadio === "accept"){
+    res.redirect('account-requests-accepted-confirmation')
+  }
+  if (selectedRadio === "reject"){
+    res.redirect('account-requests-process-reason')
+  }
+  else {
+    res.redirect('account-requests-process')
+  }
+
+})
+
+//create-person-entitlement.html
+router.post('/select-entitlement', function (req, res) {
+
+  const selectedRadio = req.body.entitlement;
+
+  if (selectedRadio === "s2"){
+    res.redirect('address-details')
+  }
+  if (selectedRadio === "s1"){
+    res.redirect('uk-address')
+  }
+  if (selectedRadio === "ehic"){
+    res.redirect('create-person-entitlement')
+  }
+  if (selectedRadio === "prc"){
+    res.redirect('create-person-entitlement')
+  }
+  else {
+    res.redirect('create-person-entitlement')
+  }
+
+})
+
+//have-s1.html
+router.post('/have-s1', function (req, res) {
+
+  const selectedRadio = req.body.s1;
+
+  if (selectedRadio === "yes"){
+    res.redirect('s1-details')
+  }
+  if (selectedRadio === "no"){
+    res.redirect('request-s1')
+  }
+  else {
+    res.redirect('have-s1')
+  }
+
+})
+
+
+module.exports = router;
