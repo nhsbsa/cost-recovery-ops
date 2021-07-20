9057a8c2095a6a589f93b9fa5652aa7d0df8f19c
lakshmi Karuppiah <lakshmi.karuppiah@nhs.net>
2021-07-20 18:26:02 +0100

    request for prc and contacted ovm resub

--- app/routes.js
+++ app/routes.js
@@ -23,6 +23,6 @@ router.use('/version-11', require('./views/version-11/_routes'));
 router.use('/version-12', require('./views/version-12/_routes'));
 router.use('/version-13', require('./views/version-13/_routes'));
 router.use('/version-14', require('./views/version-14/_routes'));
-
+router.use('/version-15', require('./views/version-15/_routes'));
 
 module.exports = router;
