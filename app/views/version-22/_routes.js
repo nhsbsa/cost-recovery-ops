f6a25ab7490e9b6f5af06068a57a8121446a36de
Elaine Russell <elaine.russell4@nhs.net>
2022-07-26 14:24:45 +0100

    PB6
    Initial prototypes moved to 'Archive' folder.
    Prep work for pages to be fit for usability testing.

--- app/views/version-22/_routes.js
+++ app/views/version-22/_routes.js
@@ -6,6 +6,29 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json()); // to support JSON bodies
 router.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
+//pb6/reason-for-editing.html
+router.post('/update-person-details', function (req, res) {
+
+  const selectedRadio = req.body.editReason; 
+
+  if (selectedRadio === "Data input error"){
+    res.redirect('/version-22/pb6/data-input-error/update-details')
+  }
+
+  if (selectedRadio === "Legal name change"){
+    res.redirect('/version-22/pb6/legal-name-change/update-details')
+  }
+
+  if (selectedRadio === "Change of address"){
+    res.redirect('/version-22/pb6/change-of-address/update-details')
+  }
+
+  else {
+    res.redirect('/version-22/pb6/reason-for-editing')
+  }
+
+})
+
 
 //pb6/data-input-error/update-details-confirmation.html
 router.post('/data-input-error-update-details', function (req, res) {
