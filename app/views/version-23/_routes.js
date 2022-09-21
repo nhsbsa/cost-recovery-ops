48bc2a1992d16e88018b2b90d3c120a596f7f632
Elaine Russell <elaine.russell4@nhs.net>
2022-09-21 14:09:57 +0100

    Add PRC Entitlement - Version 2
    Screens updated to reflect findings from UR usability sessions.

--- app/views/version-23/_routes.js
+++ app/views/version-23/_routes.js
@@ -6,7 +6,31 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json()); // to support JSON bodies
 router.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
-//pb7/add-new-entitlement.html
+
+//pb7/v2/add-new-entitlement.html
+router.post('/entitlement-type-v2', function (req, res) {
+
+  const selectedRadio = req.body.entitlementType; 
+
+  if (selectedRadio === "S2/E112"){
+    res.redirect('/version-23/pb7/v2/add-new-entitlement')
+  }
+
+  if (selectedRadio === "EHIC"){
+    res.redirect('/version-23/pb7/v2/add-new-entitlement')
+  }
+
+  if (selectedRadio === "PRC"){
+    res.redirect('/version-23/pb7/v2/add-prc-details')
+  }
+
+  else {
+    res.redirect('/version-23/pb7/v2/add-new-entitlement')
+  }
+
+})
+
+//pb7/v1/add-new-entitlement.html
 router.post('/entitlement-type', function (req, res) {
 
   const selectedRadio = req.body.entitlementType; 
