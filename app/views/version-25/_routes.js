a0debf92608ab1211fd101c83a682099dce819e5
Robert Wilson <robert.wilson23@nhs.net>
2023-06-29 14:19:28 +0100

    changed S1 landing page and fixed home button

--- app/views/version-25/_routes.js
+++ app/views/version-25/_routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 
-router.use(bodyParser.json()); // to support JSON bodies
+router.use(bodyParser.json()); // to support JS0N bodies
 router.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
 
