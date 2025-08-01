33bbd71dbe129dfe0318a0702a20fd76e58a2043
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2025-08-01 15:41:39 +0100

    Merging all the templates in preparation for upgrading.

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -15,7 +15,6 @@ class Tabs {
     this.showEvent = new CustomEvent('tab.show');
     this.hideEvent = new CustomEvent('tab.hide');
   }
-
   init() {
     if (typeof window.matchMedia === 'function' && this.responsive) {
       this.setupResponsiveChecks();
@@ -23,7 +22,6 @@ class Tabs {
       this.setup();
     }
   }
-
   setupResponsiveChecks() {
     // $mq-breakpoints: (
     // mobile: 320px,
@@ -35,7 +33,6 @@ class Tabs {
     this.mql.addEventListener('change', this.checkMode.bind(this));
     this.checkMode();
   }
-
   checkMode() {
     if (this.mql.matches) {
       this.setup();
@@ -43,7 +40,6 @@ class Tabs {
       this.teardown();
     }
   }
-
   setup() {
     const {
       $module
@@ -53,39 +49,41 @@ class Tabs {
     } = this;
     const $tabList = $module.querySelector(`.${this.namespace}__list`);
     const $tabListItems = $module.querySelectorAll(`.${this.namespace}__list-item`);
-
     if (!$tabs || !$tabList || !$tabListItems) {
       return;
     }
-
     $tabList.setAttribute('role', 'tablist');
     $tabListItems.forEach($item => {
       $item.setAttribute('role', 'presentation');
     });
     $tabs.forEach($tab => {
       // Set HTML attributes
-      this.setAttributes($tab); // Save bounded functions to use when removing event listeners during teardown
+      this.setAttributes($tab);
+
+      // Save bounded functions to use when removing event listeners during teardown
       // eslint-disable-next-line no-param-reassign
+      $tab.boundTabClick = this.onTabClick.bind(this);
+      // eslint-disable-next-line no-param-reassign
+      $tab.boundTabKeydown = this.onTabKeydown.bind(this);
 
-      $tab.boundTabClick = this.onTabClick.bind(this); // eslint-disable-next-line no-param-reassign
-
-      $tab.boundTabKeydown = this.onTabKeydown.bind(this); // Handle events
-
+      // Handle events
       $tab.addEventListener('click', $tab.boundTabClick, true);
-      $tab.addEventListener('keydown', $tab.boundTabKeydown, true); // Remove old active panels
+      $tab.addEventListener('keydown', $tab.boundTabKeydown, true);
 
+      // Remove old active panels
       this.hideTab($tab);
-    }); // Show either the active tab according to the URL's hash or the first tab
+    });
 
+    // Show either the active tab according to the URL's hash or the first tab
     const $activeTab = this.getTab(window.location.hash) || this.$tabs[0];
-    this.showTab($activeTab); // Handle hashchange events
+    this.showTab($activeTab);
 
+    // Handle hashchange events
     if (this.historyEnabled) {
       $module.boundOnHashChange = this.onHashChange.bind(this);
       window.addEventListener('hashchange', $module.boundOnHashChange, true);
     }
   }
-
   teardown() {
     const {
       $module
@@ -95,11 +93,9 @@ class Tabs {
     } = this;
     const $tabList = $module.querySelector(`.${this.namespace}__list`);
     const $tabListItems = $module.querySelectorAll(`.${this.namespace}__list-item`);
-
     if (!$tabs || !$tabList || !$tabListItems) {
       return;
     }
-
     $tabList.removeAttribute('role');
     $tabListItems.forEach($item => {
       $item.removeAttribute('role', 'presentation');
@@ -107,54 +103,48 @@ class Tabs {
     $tabs.forEach($tab => {
       // Remove events
       $tab.removeEventListener('click', $tab.boundTabClick, true);
-      $tab.removeEventListener('keydown', $tab.boundTabKeydown, true); // Unset HTML attributes
+      $tab.removeEventListener('keydown', $tab.boundTabKeydown, true);
 
+      // Unset HTML attributes
       this.unsetAttributes($tab);
     });
-
     if (this.historyEnabled) {
       // Remove hashchange event handler
       window.removeEventListener('hashchange', $module.boundOnHashChange, true);
     }
   }
-
   onHashChange() {
     const {
       hash
     } = window.location;
     const $tabWithHash = this.getTab(hash);
-
     if (!$tabWithHash) {
       return;
-    } // Prevent changing the hash
+    }
 
-
+    // Prevent changing the hash
     if (this.changingHash) {
       this.changingHash = false;
       return;
-    } // Show either the active tab according to the URL's hash or the first tab
+    }
 
-
+    // Show either the active tab according to the URL's hash or the first tab
     const $previousTab = this.getCurrentTab();
     this.hideTab($previousTab);
     this.showTab($tabWithHash);
     $tabWithHash.focus();
   }
-
   hideTab($tab) {
     this.unhighlightTab($tab);
     this.hidePanel($tab);
   }
-
   showTab($tab) {
     this.highlightTab($tab);
     this.showPanel($tab);
   }
-
   getTab(hash) {
     return this.$module.querySelector(`.${this.namespace}__tab[href="${hash}"]`);
   }
-
   setAttributes($tab) {
     // set tab attributes
     const panelId = Tabs.getHref($tab).slice(1);
@@ -162,35 +152,34 @@ class Tabs {
     $tab.setAttribute('role', 'tab');
     $tab.setAttribute('aria-controls', panelId);
     $tab.setAttribute('aria-selected', 'false');
-    $tab.setAttribute('tabindex', '-1'); // set panel attributes
+    $tab.setAttribute('tabindex', '-1');
 
+    // set panel attributes
     const $panel = this.getPanel($tab);
     $panel.setAttribute('role', 'tabpanel');
     $panel.setAttribute('aria-labelledby', $tab.id);
     $panel.classList.add(this.jsHiddenClass);
   }
-
   unsetAttributes($tab) {
     // unset tab attributes
     $tab.removeAttribute('id');
     $tab.removeAttribute('role');
     $tab.removeAttribute('aria-controls');
     $tab.removeAttribute('aria-selected');
-    $tab.removeAttribute('tabindex'); // unset panel attributes
+    $tab.removeAttribute('tabindex');
 
+    // unset panel attributes
     const $panel = this.getPanel($tab);
     $panel.removeAttribute('role');
     $panel.removeAttribute('aria-labelledby');
     $panel.removeAttribute('tabindex');
     $panel.classList.remove(this.jsHiddenClass);
   }
-
   onTabClick(e) {
     if (!e.target.classList.contains(`${this.namespace}__tab`)) {
       e.stopPropagation();
       e.preventDefault();
     }
-
     e.preventDefault();
     const $newTab = e.target;
     const $currentTab = this.getCurrentTab();
@@ -198,12 +187,12 @@ class Tabs {
     this.showTab($newTab);
     this.createHistoryEntry($newTab);
   }
-
   createHistoryEntry($tab) {
     if (this.historyEnabled) {
-      const $panel = this.getPanel($tab); // Save and restore the id
-      // so the page doesn't jump when a user clicks a tab (which changes the hash)
+      const $panel = this.getPanel($tab);
 
+      // Save and restore the id
+      // so the page doesn't jump when a user clicks a tab (which changes the hash)
       const {
         id
       } = $panel;
@@ -213,7 +202,6 @@ class Tabs {
       $panel.id = id;
     }
   }
-
   onTabKeydown(e) {
     switch (e.keyCode) {
       case this.keys.left:
@@ -221,26 +209,21 @@ class Tabs {
         this.activatePreviousTab();
         e.preventDefault();
         break;
-
       case this.keys.right:
       case this.keys.down:
         this.activateNextTab();
         e.preventDefault();
         break;
-
       default:
     }
   }
-
   activateNextTab() {
     const currentTab = this.getCurrentTab();
     const nextTabListItem = currentTab.parentNode.nextElementSibling;
     let nextTab;
-
     if (nextTabListItem) {
       nextTab = nextTabListItem.querySelector(`.${this.namespace}__tab`);
     }
-
     if (nextTab) {
       this.hideTab(currentTab);
       this.showTab(nextTab);
@@ -248,16 +231,13 @@ class Tabs {
       this.createHistoryEntry(nextTab);
     }
   }
-
   activatePreviousTab() {
     const currentTab = this.getCurrentTab();
     const previousTabListItem = currentTab.parentNode.previousElementSibling;
     let previousTab;
-
     if (previousTabListItem) {
       previousTab = previousTabListItem.querySelector(`.${this.namespace}__tab`);
     }
-
     if (previousTab) {
       this.hideTab(currentTab);
       this.showTab(previousTab);
@@ -265,50 +245,44 @@ class Tabs {
       this.createHistoryEntry(previousTab);
     }
   }
-
   getPanel($tab) {
     const $panel = this.$module.querySelector(Tabs.getHref($tab));
     return $panel;
   }
-
   showPanel($tab) {
     const $panel = this.getPanel($tab);
     $panel.classList.remove(this.jsHiddenClass);
     $panel.dispatchEvent(this.showEvent);
   }
-
   hidePanel(tab) {
     const $panel = this.getPanel(tab);
     $panel.classList.add(this.jsHiddenClass);
     $panel.dispatchEvent(this.hideEvent);
   }
-
   unhighlightTab($tab) {
     $tab.setAttribute('aria-selected', 'false');
     $tab.parentNode.classList.remove(`${this.namespace}__list-item--selected`);
     $tab.setAttribute('tabindex', '-1');
   }
-
   highlightTab($tab) {
     $tab.setAttribute('aria-selected', 'true');
     $tab.parentNode.classList.add(`${this.namespace}__list-item--selected`);
     $tab.setAttribute('tabindex', '0');
   }
-
   getCurrentTab() {
     return this.$module.querySelector(`.${this.namespace}__list-item--selected .${this.namespace}__tab`);
-  } // this is because IE doesn't always return the actual value but a relative full path
+  }
+
+  // this is because IE doesn't always return the actual value but a relative full path
   // should be a utility function most prob
   // http://labs.thesedays.com/blog/2010/01/08/getting-the-href-value-with-jquery-in-ie/
-
-
   static getHref($tab) {
     const href = $tab.getAttribute('href');
     const hash = href.slice(href.indexOf('#'), href.length);
     return hash;
   }
-
 }
+
 /**
  * Main function to invoke tabs. Can be called as follows to alter various features
  *
@@ -316,9 +290,7 @@ class Tabs {
  * Tabs({responsive: false});
  * Tabs({namespace: 'my-custom-namespace'});  // Alters classes allowing alternative css
  */
-
-
-export default (({
+export default ({
   namespace = 'nhsuk-tabs',
   responsive = true,
   historyEnabled = true
@@ -327,4 +299,4 @@ export default (({
   tabs.forEach(el => {
     new Tabs(el, namespace, responsive, historyEnabled).init();
   });
-});
+};
